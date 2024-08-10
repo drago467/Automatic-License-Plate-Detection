7e1234eda92a74668acaae1db2fd30d56e7f8b64
drago467 <tdt.trandinhtuan467@gmail.com>
2024-08-10 11:40:58 +0700

    create csv file

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from ultralytics import YOLO
 import cv2
+
+import util
 from sort.sort import *
-from util import get_car
+from util import get_car, read_license_plate, write_csv
+
+results = {}
 
 mot_tracker = Sort()
 
@@ -18,9 +22,10 @@ vehicles = [2, 3, 5, 7]
 frame_nmr = -1
 ret = True
 while ret:
-    frame_nmr = -1
+    frame_nmr += 1
     ret, frame = cap.read()
-    if ret and frame_nmr < 10:
+    if ret:
+        results[frame_nmr] = {}
         # detections vehicle
         detections = coco_model(frame)[0]
         detections_ = []
@@ -37,12 +42,29 @@ while ret:
         for license_plate in license_plates.boxes.data.tolist():
             x1, y1, x2, y2, score, class_id = license_plate
 
+
             # assign license plate to car
             xcar1, ycar1, xcar2, ycar2, car_id = get_car(license_plate, track_ids)
 
-            # crop license plate
-            license_plate_crop = frame[int(y1):int(y2), int(x1):int(x2), :]
 
-            # process license plate
-            license_plate_c
+            if car_id != -1:
 
+                # crop license plate
+                license_plate_crop = frame[int(y1):int(y2), int(x1):int(x2), :]
+
+                # process license plate
+                license_plate_crop_gray = cv2.cvtColor(license_plate_crop, cv2.COLOR_BGRA2GRAY)
+                _, license_plate_crop_thresh = cv2.threshold(license_plate_crop_gray, 64, 255, cv2.THRESH_BINARY_INV)
+
+                # read license plate
+                license_plate_text, license_plate_text_score = read_license_plate(license_plate_crop_thresh)
+
+                if license_plate_text is not None:
+                    results[frame_nmr][car_id] = {'car': {'bbox': [xcar1, ycar1, xcar2, ycar2]},
+                                                  'license_plate': {'bbox': [x1, y1,  x2, y2],
+                                                                    'text':license_plate_text,
+                                                                    'bbox_score': score,
+                                                                    'text_score': license_plate_text_score}}
+
+# write results
+write_csv(results, './test.csv')
